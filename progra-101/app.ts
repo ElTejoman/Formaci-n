2801ab8816fc57299aba99d9a566a8c12475928f
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-10 18:05:10 -0500

    objetos literales

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,2 +1,14 @@
 
-console.log('Turismo');
+let person = {
+  name: 'Melissa',
+  age: 30,
+  isActive: true,
+  hobbies: ['soccer', 'baseball'],
+  toString() {
+    let objectString = this.name + ' ' + this.age + ' ' + this.hobbies;
+    console.log( objectString )
+  }
+}
+
+//console.log(person.name.toUpperCase());
+person.toString();
