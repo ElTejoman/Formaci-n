1328f7ac6b4c52e6facda7cc36bc7b4cecc5a799
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-10 15:27:52 -0500

    refinando arrays con for

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,12 +1,22 @@
 
-let people: string[] = ['Alejandro', 'YuMei', 'Mikasa']; //Arreglo de string
-let flowers: string[] = ['Rosa', 'Girasol', 'Lirio'];
+let people: string[] = ['Alejandro', 'YuMei', 'Mikasa', 'Ruberto']; //Arreglo de string
+let salaries : number[] = [15000, 2000 ,2500]
+//let flowers: string[] = ['Rosa', 'Girasol', 'Lirio','Margarita', 'Tulipan'];
 
-for (let i = 0; i <= 5; i++){
+//for (let i = 0; i <= 5; i++){
   
-  if (flowers[i] === undefined) {
-    console.log('espacios vacios en el arreglo')
+//  if (flowers[i] === undefined) {
+//    console.log('espacios vacios en el arreglo')
+//  } else {
+//    console.log(flowers[i]);
+//  }
+//}
+
+
+for (let i = 0; i < people.length; i++){
+  if (salaries[i] === undefined) {
+    console.log(people[i] + " aún no tiene salario");
   } else {
-    console.log(flowers[i]);
+    console.log(people[i] +' tiene un salario de '+ salaries[i]);
   }
 }
