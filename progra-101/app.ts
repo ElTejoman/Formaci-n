cebf15ab0a9434d3747a56bf5dc267a7b31dea72
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-10 18:18:50 -0500

    creando objetos - ejercicio

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -11,4 +11,37 @@ let person = {
 }
 
 //console.log(person.name.toUpperCase());
-person.toString();
+//person.toString();
+
+
+let car = {
+  color: 'red',
+  numDoors: 5,
+  extraTire: 1,
+  dashCam: true,
+  airBag: true,
+  babySeat: false,
+  year: 2024,
+  brand: 'Ford',
+  model: 'Bronco',
+}
+
+let smartTv = {
+  size: '60 inch',
+  year: 2022,
+  brand: 'Samsung',
+  color: 'Platinium',
+  smartTv: true,
+}
+
+let youtubeVideo = {
+  totalTime: '3.24min',
+  short: false,
+  monetization: true,
+  likes: 45678
+}
+
+
+console.log(car);
+console.log(smartTv);
+console.log(youtubeVideo);
