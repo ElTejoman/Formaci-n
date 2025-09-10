1e2f67a091fe4d2d1529f840e5a56ba0aa5def3c
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-10 11:45:41 -0500

    primeras pruebas tarea for

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,1 +1,22 @@
-console.log('hola mundo de nuevo')
+
+//export let base: number = 10; //obligatoria
+//export let limit: number = 50; //defecto 10
+import { printMultiplicationTable } from "./helpers/print-multiplication.table";
+
+
+console.log(printMultiplicationTable(3,4));
+
+
+
+//let i = 1;
+
+//while (i <= limit) {
+//  console.log(base + "x" + i + "=", base * i);
+//  i++;
+//}
+
+//let i = 1;
+//do {
+//  console.log(base + "x" + i + "=", base * i);
+//  i++;
+//} while (i <= limit)
---
+++ progra-101/helpers/print-multiplication.table.ts
@@ -0,0 +1,19 @@
+export function printMultiplicationTable(
+  base: number,
+  limit: number = 10)
+{
+
+  for (let i = 1; i <= limit; i++){
+    if (i>0) {
+      return base * limit
+    }
+    
+
+  }
+
+
+  
+  let result = base * limit;
+  return result;
+}
+
