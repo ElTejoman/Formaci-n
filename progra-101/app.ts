ae3c38c794644209e893e8a5c03c01c54776e674
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-10 15:06:38 -0500

    ejercicio arreglo

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,1 +1,12 @@
-console.log('Hola mundo');
+
+let people: string[] = ['Alejandro', 'YuMei', 'Mikasa']; //Arreglo de string
+let flowers: string[] = ['Rosa', 'Girasol', 'Lirio'];
+
+for (let i = 0; i <= 5; i++){
+  
+  if (flowers[i] === undefined) {
+    console.log('espacios vacios en el arreglo')
+  } else {
+    console.log(flowers[i]);
+  }
+}
