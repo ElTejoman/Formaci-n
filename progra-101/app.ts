57fa74e90e031cc9852f2765de246349064412b1
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-15 15:28:13 -0500

    hw 03

--- progra-101/app.ts
+++ progra-101/app.ts
@@ -1,1 +1,5 @@
-console.log('Hello World!')
+let base: number = 10 ;
+let altura: number = 5;
+let result: number = base * altura;
+
+console.log('El áre de un triangulo de base: '+ base + ' y altura de: ' + altura + ' es igual a: ' + result)
