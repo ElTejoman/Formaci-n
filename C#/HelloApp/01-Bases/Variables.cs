85d693cf8ebbdfe46f1db33d36fb1cd863644434
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-16 15:43:02 -0500

    Iniciando en C#

---
+++ C#/HelloApp/01-Bases/Variables.cs
@@ -0,0 +1,10 @@
+partial class Program
+{
+  static void Variables()
+  {
+    int edad = 25;
+    string nombre = "Alejandro";
+    bool esEstudiante = true;
+    Console.WriteLine($"Nombre: {nombre} tiene una edad {edad} es estudante {esEstudiante}");
+  }
+}
