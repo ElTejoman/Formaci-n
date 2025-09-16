351f750e7e7aeb390fd47d0cd18e7d486dd0cb50
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-16 10:41:39 -0500

    fin tareas

---
+++ progra-101/homework/hw-9.ts
@@ -0,0 +1,32 @@
+
+
+
+
+export function heroesThatStartsWith(nameList: string[]) {
+  
+  letter = letter.toUpperCase();
+  let newNameList: string[] = [];
+
+  for (let i = 0; nameList.length; i++){
+    let name = nameList[i];
+
+    if (name.startsWith(letter)) {
+      newNameList.push(name)
+    }
+  }
+
+  return newNameList
+}
+
+
+
+let heroes = ['Doom', 'Dr. Strange', 'Hulk', 'She Hulk', 'Spiderman', 'Captain Marvel'];
+let herosWithLetterS = heroesThatStartsWith( heroes, 'S' );
+
+
+
+
+
+
+
+console.log(herosWithLetterS); // She Hulk, Spiderman
