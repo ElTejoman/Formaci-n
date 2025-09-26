48883b3183ef6a5df15fa154a2e3c2f1c0620f16
ElTejoman <aespinoza.ascue@gmail.com>
2025-09-26 16:33:44 -0500

    arrays pt 2

--- C#/HelloApp/01-Bases/Arrays.cs
+++ C#/HelloApp/01-Bases/Arrays.cs
@@ -6,7 +6,7 @@ partial class Program
     numbers[0] = 1;
     numbers[1] = 3;
     //Directa
-    int[] numbersArray = [5, 10, 15, 20, 25];
+    int[] numbersArray = [5, 10, 15, 20, 25, 30];
 
     //indices
     Console.WriteLine($"Primer elemento {numbersArray[0]}");
@@ -14,5 +14,21 @@ partial class Program
 
     //tamano arreglo
     Console.WriteLine($"El numero de elementos son {numbersArray.Length}");
+
+    // Desde el final el arreglo
+    Console.WriteLine($"Ultimo elemento es {numbersArray[^1]}");  // ultimo
+    Console.WriteLine($"Primer elemento {numbersArray[^2]}");   //penultimo
+
+    // Rangos para obtener subarreglos .. 
+    int[] firstThree = numbersArray[..3];
+    int[] fromIndexTwo = numbersArray[2..];
+
+    foreach (var number in fromIndexTwo)
+    {
+      Console.WriteLine(number);
+    }
+
+
+
   }
 }
