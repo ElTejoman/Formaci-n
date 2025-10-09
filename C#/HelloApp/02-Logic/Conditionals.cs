de90e30469344c15da1ae09333a5f367a9c0f87c
ElTejoman <aespinoza.ascue@gmail.com>
2025-10-09 01:10:10 -0500

    conditionals elseif

--- C#/HelloApp/02-Logic/Conditionals.cs
+++ C#/HelloApp/02-Logic/Conditionals.cs
@@ -6,6 +6,32 @@ partial class Program
     {
       WriteLine($"Felicidades, por tener {age}, ya eres mayor de edad");
     }
+    else
+    {
+      WriteLine($"Eres menor de edad");
+    }
+
+    //if ternario
+
+    string message = age >= 18 ? "Eres mayora de edad" : "Eres menor de edad";
+    WriteLine(message);
+    // multiples condiciones
+
+    int temperature = 30;
+
+    if (temperature > 35)
+    {
+      WriteLine("Hace mucho calor");
+    }
+    else if (temperature >= 20)
+    {
+      WriteLine("es agradable");
+    }
+    else
+    {
+        WriteLine("Hace frio");
+      
+    }
 
   }
 }
