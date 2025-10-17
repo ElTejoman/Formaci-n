54cb4bfb94e8cc3852452b45656e9942e31fe4c0
ElTejoman <aespinoza.ascue@gmail.com>
2025-10-17 00:04:18 -0500

    foreach

--- C#/HelloApp/02-Logic/Loops.cs
+++ C#/HelloApp/02-Logic/Loops.cs
@@ -1,4 +1,6 @@
 
+using System.Threading.Channels;
+
 partial class Program
 {
   static void Loops()
@@ -6,7 +8,7 @@ partial class Program
     int counter = 1;
     while (counter <= 5)
     {
-      WriteLine($"Iteraacion: {counter}");
+      // WriteLine($"Iteraacion: {counter}");
       counter++;
     }
 
@@ -14,9 +16,28 @@ partial class Program
     int number = 0;
     do
     {
-      WriteLine($"Nuero: {number}");
+      // WriteLine($"Nuero: {number}");
       number++;
     } while (number < 5);
+
+
+    //bucle for
+
+    for (int i = 0; i < 5; i++)
+    {
+      WriteLine($"Iteracion {i}");
+    }
+
+
+    // foreach
+    // array
+
+    string[] fruitname = ["Manzana", "Pera", "Pina"];
+
+    foreach (var fruit in fruitname)
+    {
+      WriteLine(fruit);
+    }
   }
   
 }
