b660b15ab21c555afe9637881e65a1136df691d8
ElTejoman <aespinoza.ascue@gmail.com>
2025-11-03 22:17:50 -0500

    Methods part 2

--- C#/HelloApp/03-Classes/Methods.cs
+++ C#/HelloApp/03-Classes/Methods.cs
@@ -2,6 +2,14 @@ partial class Program
 {
   static void Methods()
   {
+    Car car = new Car();
+    car.Model = "Toyota";
+    car.Year = 2020;
+    WriteLine(car.ShowInfo());
+    car.ShowMessage("Hello, Car!");
+    car.ChangeModel("Honda");
+    WriteLine(car.ShowInfo());
+    Car.GeneralInfo();
 
   }
 }
@@ -18,4 +26,6 @@ class Car
   {
     return $"Model: {Model}, Year: {Year}";
   }
+  public void ShowMessage(string message) => WriteLine("This is a car.");
+  public static void GeneralInfo() => WriteLine("Cars are vehicles used for transportation.");
 }
